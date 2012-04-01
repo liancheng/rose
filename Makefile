0e9f6e4a31430601baa188b98b25416156bbc3de
Lian Cheng <rhythm.mail (at) gmail.com>
2012-04-01 17:11:17 +0800

    Initial commit.

---
+++ Makefile
@@ -0,0 +1,38 @@
+all: sexp
+
+sexp: sexp.o
+	clang 					\
+		sexp.o lexer/r5rs_lexer.o	\
+		-o sexp				\
+		-L/lib				\
+		-lreadline
+
+sexp.o: lexer sexp.c r5rs.qx
+	quex					\
+		-i r5rs.qx			\
+		-o r5rs_lexer			\
+		--language C			\
+		--token-prefix TKN_		\
+		--output-directory lexer	\
+		--token-policy single		\
+		--token-memory-management-by-user
+
+	clang				\
+		-c lexer/r5rs_lexer.c	\
+		-o lexer/r5rs_lexer.o	\
+		-I./lexer		\
+		-I.			\
+		-I$(QUEX_PATH)		\
+		-DQUEX_OPTION_ASSERTS_WARNING_MESSAGE_DISABLED
+
+	clang			\
+		-c sexp.c	\
+		-o sexp.o	\
+		-I.		\
+		-I$(QUEX_PATH)
+
+clean:
+	rm -f sexp.o sexp
+	rm -rf lexer/*
+
+# vim:ft=make ts=8 sw=8 noet
---
+++ sexp.h
@@ -0,0 +1,57 @@
+#ifndef __ROSE_SEXP_H__
+#define __ROSE_SEXP_H__
+
+enum sexp_types {
+    SEXP_BOOLEAN,
+    SEXP_PAIR,
+    SEXP_SYMBOL,
+};
+
+typedef struct sexp_struct* sexp;
+
+struct sexp_struct {
+    char tag;
+
+    union {
+        struct {
+            sexp car;
+            sexp cdr;
+        }
+        pair;
+
+        struct {
+            unsigned long length;
+            char name[];
+        }
+        symbol;
+    }
+    value;
+};
+
+/**
+ * Simple tagging.  Ends in:
+ *
+ * - 0000 ( 0): pointer
+ * - 0011 ( 3): long symbol
+ * - 0111 ( 7): immediate symbol
+ * - 1110 (14): other immediate constants ('(), #t, #f, etc.)
+ */
+#define SEXP_POINTER_TAG    0x00
+#define SEXP_L_SYMBOL_TAG   0x03
+#define SEXP_I_SYMBOL_TAG   0x07
+
+#define MAKE_IMMEDIATE(n)   ((sexp)((n << 4) + 0x0e))
+#define SEXP_NULL           MAKE_IMMEDIATE(0)
+#define SEXP_FALSE          MAKE_IMMEDIATE(1)
+#define SEXP_TRUE           MAKE_IMMEDIATE(2)
+#define SEXP_EOF            MAKE_IMMEDIATE(3)
+#define SEXP_UNDEFINED      MAKE_IMMEDIATE(4)
+
+#define SEXP_NULL_P(s)      ((s) == SEXP_NULL)
+#define SEXP_POINTER_P(s)   (((unsigned long)(s) & 0x03) == SEXP_POINTER_TAG)
+#define SEXP_I_SYMBOL_P(s)  (((unsigned long)(s) & 0x07) == SEXP_I_SYMBOL_P)
+#define SEXP_L_SYMBOL_P(s)  (((unsigned long)(s) & 0x07) == SEXP_L_SYMBOL_P)
+#define SEXP_SYMBOL_P(s)    (SEXP_I_SYMBOL_P(s) || SEXP_L_SYMBOL_P(s))
+#define SEXP_PAIR_P(s)      (SEXP_POINTER_P(s) && ((sexp)(s))->tag == SEXP_PAIR)
+
+#endif  //  __ROSE_SEXP_H__
