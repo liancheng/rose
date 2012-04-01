64d920166cab215ce35fae12bc3310735303d808
Lian Cheng <liancheng404@gmail.com>
2012-04-02 01:12:39 +0800

    Added command line options handling.

--- sexp.cpp
+++ sexp.cpp
@@ -1,14 +1,49 @@
 #include "sexp.h"
 #include "lexer/r5rs_lexer"
 
+#include <getopt.h>
 #include <readline/history.h>
 #include <readline/readline.h>
 
 #include <stdio.h>
+#include <string>
+
+static struct option long_options[] = {
+    {"input", required_argument, 0, 'i'},
+};
 
 int main(int argc, char* argv[])
 {
-    quex::r5rs_lexer qlex(argv[1]);
+    std::string input;
+
+    while (true) {
+        int option_index = 0;
+        int opt = getopt_long(argc,
+                              argv,
+                              "i:",
+                              long_options,
+                              &option_index);
+
+        if (-1 == opt) {
+            break;
+        }
+
+        switch (opt) {
+            case 'i':
+                input = optarg;
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    if (input.empty()) {
+        fprintf(stderr, "no input file.\n");
+        return EXIT_FAILURE;
+    }
+
+    quex::r5rs_lexer qlex(input.c_str());
     quex::Token* token = NULL;
 
     do {
