377a7f8292804960860a62fdaed04e2c386c7129
Lian Cheng <rhythm.mail@gmail.com>
2012-05-04 12:10:25 +0800

    Added test input source file and expected output.

---
+++ test/expected.scm
@@ -0,0 +1,18 @@
+a
+#t
+#f
+"#f"
+"hello world"
+(quote a)
+(quasiquote a)
+(unquote a)
+(unquote-splicing a)
+()
+(a)
+(a . b)
+(a b . c)
+(a b)
+(a b)
+#()
+#(a)
+#(a b)
---
+++ test/test.scm
@@ -0,0 +1,18 @@
+a
+#t
+#f
+""
+"hello world"
+'a
+`a
+,a
+,@a
+()
+(a)
+(a . b)
+(a b . c)
+(a b)
+(a b . ())
+#()
+#(a)
+#(a b)
