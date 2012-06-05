e001e9246f27498dc3628ff59cc120c621d12dc5
Lian Cheng <rhythm.mail@gmail.com>
2012-06-05 21:19:38 +0800

    Update test case.

--- test/expected.scm
+++ test/expected.scm
@@ -1,1 +1,17 @@
-(#t #f "" "hello \"world\"" (quote a) (quasiquote a) (unquote a) (unquote-splicing a) () (a) (a . b) (a b . c) (a b) (a b) #() #(a) #(a b))
+#t
+#f
+""
+"hello \"world\""
+(quote a)
+(quasiquote a)
+(unquote a)
+(unquote-splicing a)
+()
+(a)
+(a . b)
+(a b . c)
+(a b)
+(a b)
+#()
+#(a)
+#(a b)
