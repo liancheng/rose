cd404ebede15018c42048cd51f72df841e31261f
Lian Cheng <rhythm.mail@gmail.com>
2013-01-17 17:13:20 +0800

    Added several number primitive procedures

--- src/number.c
+++ src/number.c
@@ -1,11 +1,14 @@
 #include "detail/number.h"
 #include "detail/number_reader.h"
 #include "detail/port.h"
+#include "detail/primitive.h"
 #include "detail/sexp.h"
 #include "detail/state.h"
 #include "rose/error.h"
 #include "rose/gmp.h"
+#include "rose/pair.h"
 #include "rose/port.h"
+#include "rose/string.h"
 
 #include <assert.h>
 #include <string.h>
@@ -108,6 +111,43 @@ static RFixnum* fixnum_new (RState* r)
 
     mpq_inits (fixnum->real, fixnum->imag, NULL);
     return fixnum;
+}
+
+static rsexp np_number_p (RState* r, rsexp args)
+{
+    return r_bool_to_sexp (r_number_p (r_car (args)));
+}
+
+static rsexp np_exact_p (RState* r, rsexp args)
+{
+    return r_bool_to_sexp (r_exact_p (r_car (args)));
+}
+
+static rsexp np_inexact_p (RState* r, rsexp args)
+{
+    return r_bool_to_sexp (r_inexact_p (r_car (args)));
+}
+
+static rsexp np_exact_to_inexact (RState* r, rsexp args)
+{
+    return r_exact_to_inexact (r, r_car (args));
+}
+
+static rsexp np_inexact_to_exact (RState* r, rsexp args)
+{
+    return r_inexact_to_exact (r, r_car (args));
+}
+
+static rsexp np_string_to_number (RState* r, rsexp args)
+{
+    rsexp num;
+    rsexp radix;
+    rsexp res;
+
+    r_match_args (r, args, 1, 1, FALSE, &num, &radix);
+    res = r_string_to_number (r, r_car (args));
+
+    return r_failure_p (res) ? R_FALSE : res;
 }
 
 rbool r_fixnum_p (rsexp obj)
@@ -308,4 +348,14 @@ RTypeInfo flonum_type = {
         .eqv_p = flonum_eqv_p,
         .equal_p = flonum_eqv_p,
     }
+};
+
+RPrimitiveDesc number_primitives [] = {
+    { "number?",        np_number_p,         1, 0, FALSE },
+    { "exact?",         np_exact_p,          1, 0, FALSE },
+    { "inexact?",       np_inexact_p,        1, 0, FALSE },
+    { "exact->inexact", np_exact_to_inexact, 1, 0, FALSE },
+    { "inexact->exact", np_inexact_to_exact, 1, 0, FALSE },
+    { "string->number", np_string_to_number, 1, 1, FALSE },
+    { NULL }
 };
